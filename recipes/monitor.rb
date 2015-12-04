6c778ffe8974356d4ddb0512e72b605a45eff13f
Jim Dowling <jdowling@sics.se>
2015-12-04 13:31:34 +0100

    add monitor recipe

---
+++ recipes/monitor.rb
@@ -0,0 +1,30 @@
+
+monitordir = "/srv/kafka-monitor"
+directory monitordir do
+  owner node[:kzookeeper][:user]
+  group node[:kafka][:group]
+  mode "0774"
+  recursive true
+  action :create
+  not_if { File.directory?("#{monitordir}") }
+end
+
+url = node[:kkafka][:offset_monitor][:url]
+base_filename = File.basename(url)
+dest="#{monitordir}/#{base_filename}"
+
+remote_file dest do
+  source url
+  owner node[:kzookeeper][:user]
+  group node[:kafka][:group]
+  mode "0775"
+  action :create_if_missing
+end
+
+zk_ips = node[:kzookeeper][:default][:private_ips].join(",")
+
+kkafka_monitor "kafka-monitor" do
+ jar_file File.basename(node[:kkafka][:offset_monitor][:url])
+ zk_ips zk_ips
+ action :start
+end
