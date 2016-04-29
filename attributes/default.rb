2460022ea44a350f5705f58470743bdaeddb0629
Kamal Hakimzadeh <k.hakimzadeh@gmail.com>
2016-04-29 14:30:39 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 include_attribute "kafka"
 include_attribute "flink"
-include_attribute "spark"
-include_attribute "hadoop"
+include_attribute "hadoop_spark"
+include_attribute "apache_hadoop"
 include_attribute "kzookeeper"
 
 default[:kafka][:user] = "kafka"
@@ -18,4 +18,4 @@ node.normal.kafka.version = "0.8.2.2"
 node.normal.kafka.checksum = "6e75bb0e0438960aa668d0f9d570009647e457d74c377e4bbc508cec43ecf639"
 node.normal.kafka.md5_checksum = "77e4fba83e8b5e8a2d63785980fde393"
 
-node.normal.kafka.partitions = 
+node.normal.kafka.partitions = 2
